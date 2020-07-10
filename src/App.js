0f695fa553795a1333364d6b40737a50902ae025
Jonathan Cleary <jcleary@MacBook-Pro.local>
2020-07-10 13:30:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { Link, Router } from "@reach/router";
 
 function App() {
+  const Home = () => (
+    <div>
+      <h2>
+        Time Flies like an Arrow,
+        <br />
+        Fruit Flies like a Banana
+      </h2>
+    </div>
+  );
+
+  const Dashboard = () => (
+    <div>
+      <h2>You are Broadcasting video and audio</h2>
+    </div>
+  );
+
+  const Invoice = (props) => (
+    <div>
+      <h2>You are tuning in to a Broadcast</h2>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TokBox Proof of Concept!</h1>
+      <nav>
+        <Link to="/">Home</Link> <Link to="dashboard">Broadcast</Link>{" "}
+        <Link to="invoice">Tune in!</Link>
+      </nav>
+
+      <Router>
+        <Home path="/" />
+        <Dashboard path="/dashboard" />
+        <Invoice path="invoice" />
+      </Router>
     </div>
   );
 }
